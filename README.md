f2669b5e8dad4e296d089d5ca1b0027220314df1
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-03-07 14:12:07 -0300

    Projeto CRM API Typescript

---
+++ README.md
@@ -0,0 +1,14 @@
+# CRM API with NODEJS + Typescript + MongoDB
+
+## Technologies
+    - NodeJS
+    - Typescript
+    - Fastify
+    - MongoDB
+    - Prisma
+
+### Run software (dev mode)
+    npm run dev
+
+### Acessando API
+    Teste: localhost:3333/teste
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import fastify from "fastify";
+import { routes } from "./routes";
+import cors from "@fastify/cors"
+
+const PORT = 3333
+
+const app = fastify({ logger: true });
+
+const start = async () => {
+
+    await app.register(cors);
+    await app.register(routes);
+
+    try {
+        await app.listen({ port: PORT })
+    } catch (err) {
+        process.exit(1);
+    }
+}
+
+start();
